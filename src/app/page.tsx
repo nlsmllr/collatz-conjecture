f1ea5f70770ec78e4993b84658181f8fc0ed2020
nlsmllr <info@nils-mueller.com>
2024-12-02 12:23:14 +0100

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CollatzConjecture from "./Components/collatz-conjecture";
 export default function Home() {
   return (
     <div className="items-center justify-items-center p-8 pb-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col h-screen justify-center gap-8 row-start-2 items-center sm:items-start">
       <CollatzConjecture />
       </main>
      
